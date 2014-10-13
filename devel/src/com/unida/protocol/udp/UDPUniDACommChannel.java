2460e150f26f4ec93201d7f9729f645ffc581392
Victor Sonora Pombo <victor@vsonora.com>
2014-10-13 16:18:10 +0200

    Modifies CommChannel to add a constructor that handles IP.

--- devel/src/com/unida/protocol/udp/UDPUniDACommChannel.java
+++ devel/src/com/unida/protocol/udp/UDPUniDACommChannel.java
@@ -1,31 +1,29 @@
-
-/*******************************************************************************
- *   
- *   Copyright (C) 2009-2013 Mytech Ingenieria Aplicada <http://www.mytechia.com>
- *   Copyright (C) 2009-2013 Gervasio Varela <gervarela@picandocodigo.com>
- *   Copyright (C) 2012-2013 Victor Sonora <victor@vsonora.com>
- *   Copyright (C) 2009-2013 Alejandro Paz <alejandropl@gmail.com>
- * 
- *   This file is part of UNIDA.
+/**
+ * *****************************************************************************
  *
- *   UNIDA is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Affero General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * Copyright (C) 2009-2013 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright (C) 2009-2013 Gervasio Varela <gervarela@picandocodigo.com>
+ * Copyright (C) 2012-2013 Victor Sonora <victor@vsonora.com>
+ * Copyright (C) 2009-2013 Alejandro Paz <alejandropl@gmail.com>
  *
- *   UNIDA is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
+ * This file is part of UNIDA.
  *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with UNIDA.  If not, see <http://www.gnu.org/licenses/>.
- * 
- ******************************************************************************/
-
-
+ * UNIDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * UNIDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with UNIDA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************
+ */
 package com.unida.protocol.udp;
-
 
 import com.mytechia.commons.framework.simplemessageprotocol.udp.IUDPCommunicationChannel;
 import com.mytechia.commons.framework.simplemessageprotocol.channel.IAddress;
@@ -42,15 +40,18 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 
 /**
- * <p><b>
+ * <p>
+ * <b>
  * Sends and receives HI3 DAL protocol messages using UDP sockets
  * </b>
  *
  * </p>
  *
- * <p><b>Creation date:</b> 30-01-2010</p>
+ * <p>
+ * <b>Creation date:</b> 30-01-2010</p>
  *
- * <p><b>Changelog:</b>
+ * <p>
+ * <b>Changelog:</b>
  * <ul>
  * <li>1 - 30-01-2010 Initial release</li>
  * </ul>
@@ -61,81 +62,89 @@ import java.util.logging.Level;
  */
 public class UDPUniDACommChannel implements IUniDACommChannel
 {
-    
+
     private static final short UNIDA_LEAF_NUMBER = 0;
 
     private IUDPCommunicationChannel commChannel;
 
     private IMessageFactory msgFactory;
-    
-
 
     public UDPUniDACommChannel(int port, IMessageFactory msgFactory) throws CommunicationException
     {
-        try {
+        try
+        {
             this.commChannel = new UDPUniDACommunicationChannelImplementation(port);
             this.msgFactory = msgFactory;
-        }
-        catch (UnknownHostException | SocketException ex) {
+        } catch (UnknownHostException | SocketException ex)
+        {
             throw new CommunicationException(ex);
         }
     }
-
 
     public UDPUniDACommChannel(IMessageFactory msgFactory) throws CommunicationException
     {
         this(UDPUniDACommunicationChannelImplementation.UNIDA_PROTOCOL_UDP_PORT, msgFactory);
     }
-    
+
+    public UDPUniDACommChannel(String ip, IMessageFactory msgFactory) throws CommunicationException
+    {
+        try
+        {
+            this.commChannel = new UDPUniDACommunicationChannelImplementation(ip);
+            this.msgFactory = msgFactory;
+        } catch (UnknownHostException | SocketException ex)
+        {
+            throw new CommunicationException(ex);
+        }
+    }
 
     @Override
     public void sendMessage(DeviceID dev, UniDAMessage msg) throws CommunicationException
     {
-        
-        try {
-            
+
+        try
+        {
+
             this.commChannel.send(createUDPAddress(dev.getGatewayId()), msg.codeMessage());
-            
+
             UniDALoggers.LIBRARY.log(Level.FINE, "sent message: {0}", msg.toString());
-            
-        } catch (UnknownHostException ex) {
+
+        } catch (UnknownHostException ex)
+        {
             throw new CommunicationException(ex);
         }
 
     }
-    
-    
+
     @Override
     public void sendMessage(UniDAAddress gw, UniDAMessage msg) throws CommunicationException
     {
-        
-        try {
-            
+
+        try
+        {
+
             this.commChannel.send(createUDPAddress(gw), msg.codeMessage());
-            
+
             UniDALoggers.LIBRARY.log(Level.FINE, "sent message: {0}", msg.toString());
-            
-        } catch (UnknownHostException ex) {
+
+        } catch (UnknownHostException ex)
+        {
             throw new CommunicationException(ex);
         }
 
     }
-    
-    
+
     private IAddress createUDPAddress(UniDAAddress uaddr) throws UnknownHostException
     {
         return uaddr.toUDPAddress(UDPUniDACommunicationChannelImplementation.UNIDA_PROTOCOL_UDP_PORT);
     }
-    
-
 
     @Override
     public void broadcastMessage(UniDAMessage msg) throws CommunicationException
     {
         this.commChannel.broadcast(msg.codeMessage());
-        UniDALoggers.LIBRARY.log(Level.FINE, "broadcast message: {0}", msg.toString());
+        UniDALoggers.LIBRARY.log(Level.FINE, "Broadcast message: {0}", msg.toString());
     }
-
 
     @Override
     public UniDAMessage receiveMessage() throws CommunicationException
@@ -147,15 +156,13 @@ public class UDPUniDACommChannel implements IUniDACommChannel
         UniDALoggers.LIBRARY.log(Level.FINE, "Message recived: {0}", msg.toString());
 
         return msg;
-  
+
     }
 
-    
     @Override
     public UniDAAddress getAddress()
     {
         return new UniDAAddress(this.commChannel.getIPAddress().toString(), UNIDA_LEAF_NUMBER);
     }
-	
-	
+
 }
