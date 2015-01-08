920dee544d760abd550d471702e94081f6dd97bb
Victor Sonora Pombo <victor@vsonora.com>
2015-01-08 17:33:03 +0100

    processMessage() can return N responses now.

--- unida_library/src/main/java/com/unida/protocol/handling/UniDAProtocolMessageProcessor.java
+++ unida_library/src/main/java/com/unida/protocol/handling/UniDAProtocolMessageProcessor.java
@@ -1,30 +1,29 @@
-/*******************************************************************************
- *   
- *   Copyright (C) 2009-2013 Mytech Ingenieria Aplicada <http://www.mytechia.com>
- *   Copyright (C) 2009-2013 Gervasio Varela <gervarela@picandocodigo.com>
- *   Copyright (C) 2012-2013 Victor Sonora <victor@vsonora.com>
- *   Copyright (C) 2009-2013 Alejandro Paz <alejandropl@gmail.com>
- * 
- *   This file is part of UNIDA.
+/**
+ * *****************************************************************************
  *
- *   UNIDA is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Affero General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * Copyright (C) 2009-2013 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright (C) 2009-2013 Gervasio Varela <gervarela@picandocodigo.com>
+ * Copyright (C) 2012-2013 Victor Sonora <victor@vsonora.com>
+ * Copyright (C) 2009-2013 Alejandro Paz <alejandropl@gmail.com>
  *
- *   UNIDA is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Affero General Public License for more details.
+ * This file is part of UNIDA.
  *
- *   You should have received a copy of the GNU Affero General Public License
- *   along with UNIDA.  If not, see <http://www.gnu.org/licenses/>.
- * 
- ******************************************************************************/
-
-
+ * UNIDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * UNIDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with UNIDA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************
+ */
 package com.unida.protocol.handling;
-
 
 import com.mytechia.commons.framework.simplemessageprotocol.exception.CommunicationException;
 import com.unida.log.UniDALoggers;
@@ -33,13 +32,14 @@ import com.unida.protocol.message.UniDAMessage;
 import com.unida.protocol.handling.exception.UnsupportedMessageTypeErrorException;
 import com.unida.protocol.reception.IUniDAProtocolMessageProcessingQueue;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Level;
 
-
 /**
- * <p><b>
- * It is a thread in charge of providing computing resources to process
- * UniDA protocol messages.
+ * <p>
+ * <b>
+ * It is a thread in charge of providing computing resources to process UniDA
+ * protocol messages.
  *
  * It associated with a message reception queue from wich it obtains the
  * messages to process.
@@ -50,9 +50,11 @@ import java.util.logging.Level;
  *
  * </p>
  *
- * <p><b>Creation date:</b> 30-01-2010</p>
+ * <p>
+ * <b>Creation date:</b> 30-01-2010</p>
  *
- * <p><b>Changelog:</b>
+ * <p>
+ * <b>Changelog:</b>
  * <ul>
  * <li>1 - 30-01-2010 Initial release</li>
  * </ul>
@@ -68,8 +70,7 @@ public class UniDAProtocolMessageProcessor extends Thread
 
     private IUniDAProtocolMessageProcessingQueue msgQueue;
     private ArrayList<IUniDAProtocolMessageHandler> handlers;
-    private IUniDACommChannel commChannel;
-
+    protected IUniDACommChannel commChannel;
 
     public UniDAProtocolMessageProcessor(
             IUniDACommChannel commChannel, IUniDAProtocolMessageProcessingQueue msgQueue)
@@ -80,7 +81,6 @@ public class UniDAProtocolMessageProcessor extends Thread
         this.handlers = new ArrayList<>();
     }
 
-
     /**
      * @param msgHandler
      */
@@ -89,7 +89,6 @@ public class UniDAProtocolMessageProcessor extends Thread
         this.handlers.add(msgHandler);
     }
 
-
     /**
      * @param msgHandler
      */
@@ -97,7 +96,6 @@ public class UniDAProtocolMessageProcessor extends Thread
     {
         this.handlers.remove(msgHandler);
     }
-
 
     /**
      *
@@ -108,7 +106,6 @@ public class UniDAProtocolMessageProcessor extends Thread
         this.start();
     }
 
-
     /**
      *
      */
@@ -117,61 +114,75 @@ public class UniDAProtocolMessageProcessor extends Thread
         this.processing = false;
     }
 
-
-    /** Uses the available message handlers to process the new message received
+    /**
+     * Uses the available message handlers to process the new message received
+     *
      * @param msg
-     * @return 
-     * @throws com.unida.protocol.handling.exception.UnsupportedMessageTypeErrorException
+     * @return
+     * @throws
+     * com.unida.protocol.handling.exception.UnsupportedMessageTypeErrorException
      */
-    protected UniDAMessage processMessage(UniDAMessage msg) throws UnsupportedMessageTypeErrorException
+    protected Collection<UniDAMessage> processMessage(UniDAMessage msg) throws UnsupportedMessageTypeErrorException
     {
 
-        UniDAMessage response = null;
-        for(IUniDAProtocolMessageHandler h : this.handlers) {
-            if (h.supports(msg)) {
-                response = h.handle(msg);
+        Collection<UniDAMessage> responses = new ArrayList<UniDAMessage>();
+
+        for (IUniDAProtocolMessageHandler h : this.handlers)
+        {
+            if (h.supports(msg))
+            {
+                UniDAMessage response = h.handle(msg);
+                responses.add(response);
                 break;
             }
         }
 
-        return response;
+        return responses;
 
     }
-
 
     @Override
     public void run()
     {
 
-        while(this.processing) {
+        while (this.processing)
+        {
 
             UniDAMessage msg = this.msgQueue.pollMessageOrWait();
 
-            try{
+            try
+            {
 
-                UniDAMessage response = processMessage(msg);
+                Collection<UniDAMessage> responses = processMessage(msg);
 
-                if (response != null) {
-                    this.commChannel.sendMessage(msg.getSource(), response);
+                if (responses != null)
+                {
+                    for (UniDAMessage response : responses)
+                    {
+                        if (null != response)
+                        {
+                            this.commChannel.sendMessage(msg.getSource(), response);
+                        }
+                    }
+
                 }
 
-            }
-            catch (UnsupportedMessageTypeErrorException ex) {
+            } catch (UnsupportedMessageTypeErrorException ex)
+            {
                 UniDALoggers.LIBRARY.log(
-                        Level.WARNING, 
-                        "Unsupported message received: " + msg.toString(), 
+                        Level.WARNING,
+                        "Unsupported message received: " + msg.toString(),
                         ex.getLocalizedMessage());
-            }
-            catch (CommunicationException ex) {
+            } catch (CommunicationException ex)
+            {
                 UniDALoggers.LIBRARY.log(
-                        Level.SEVERE, 
-                        "Error processing message: " + msg.toString() + ". " + ex.getLocalizedMessage(), 
+                        Level.SEVERE,
+                        "Error processing message: " + msg.toString() + ". " + ex.getLocalizedMessage(),
                         ex.getLocalizedMessage());
             }
 
         }
 
     }
-
 
 }
